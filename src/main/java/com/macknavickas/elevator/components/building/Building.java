776c6a6e5afecaa02cffc07e63e3ff1ed8941654
Mack Navickas <261navickasm@student.dist113.org>
2024-07-01 15:20:06 -0500

    I am trying to fix my Github account

--- src/main/java/com/macknavickas/elevator/components/building/Building.java
+++ src/main/java/com/macknavickas/elevator/components/building/Building.java
@@ -1,13 +1,16 @@
 package com.macknavickas.elevator.components.building;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Building {
     // Riot mode
+
+    // Test1
+    private static int shaftIdIncrementer = -1;
+
     private HashMap<Integer, Shaft> shafts;
 
-    int floors;
+    private int floors;
 
     public Building() {
         shafts = new HashMap<>();
@@ -24,7 +27,22 @@ public class Building {
      * @return ID number of shaft inside building.
      */
     public int addShaft(int bottomFloor, int topFloor) {
-        shafts.put(shafts.size(), new Shaft(bottomFloor, topFloor, shafts.size()));
-        return shafts.size() - 1;
+        int id = getNextShaftId();
+        shafts.put(id, new Shaft(bottomFloor, topFloor, shafts.size()));
+        return id;
+    }
+
+    public int getNextShaftId() {
+        return shaftIdIncrementer++;
+    }
+
+    /**
+     *
+     * @return The Shaft in use or null if not in use.
+     */
+    public Shaft isIdInUse(int id) {
+        if (shafts.containsKey(id)) {
+            return shafts.get(id);
+        } else return null;
     }
 }
